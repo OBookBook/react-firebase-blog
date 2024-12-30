49483e9104ec52ef574c35af3101f1dc5696e63e
N.I <iworksWeb@gmail.com>
2024-12-31 00:10:50 +0900

    css Navbar design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav>
       <Link to={"/"}>Home</Link>
-      <Link to={"/createpost"}>post</Link>
+      <Link to={"/createpost"}>Post</Link>
       <Link to={"/login"}>Login</Link>
     </nav>
   );
