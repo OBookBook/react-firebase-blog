7a4cf2a5269e22fc342390e8ea00b3a3889bcaaa
N.I <iworksWeb@gmail.com>
2024-12-31 00:30:58 +0900

    add fontawesome React

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,27 @@
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHouse,
+  faFilePen,
+  faArrowRightToBracket,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to={"/"}>Home</Link>
-      <Link to={"/createpost"}>Post</Link>
-      <Link to={"/login"}>Login</Link>
+      <Link to={"/"}>
+        <FontAwesomeIcon icon={faHouse} />
+        Home
+      </Link>
+      <Link to={"/createpost"}>
+        <FontAwesomeIcon icon={faFilePen} />
+        Post
+      </Link>
+      <Link to={"/login"}>
+        <FontAwesomeIcon icon={faArrowRightToBracket} />
+        Login
+      </Link>
     </nav>
   );
 };
