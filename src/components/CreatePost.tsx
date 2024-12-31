c22d5377b9cca5e5c1592cc7f99ca71cc67f7211
N.I <iworksWeb@gmail.com>
2024-12-31 10:47:37 +0900

    add CreatePost component form event

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,19 +1,37 @@
+import { useState } from "react";
 import "./CreatePost.css";
 
 const CreatePost = () => {
+  const [title, setTitle] = useState<string>("");
+  const [postText, setPostText] = useState<string>("");
+
+  const CreatePost = () => {
+    console.log(title);
+    console.log(postText);
+  };
+
   return (
     <div className="createPostPage">
       <div className="postContainer">
         <h1>Post Page</h1>
         <div className="inputPost">
           <div>Title</div>
-          <input type="text" placeholder="add title" />
+          <input
+            type="text"
+            placeholder="add title"
+            onChange={(e) => setTitle(e.target.value)}
+          />
         </div>
         <div className="inputPost">
           <div>Commet</div>
-          <textarea placeholder="add comment" />
+          <textarea
+            placeholder="add comment"
+            onChange={(e) => setPostText(e.target.value)}
+          />
         </div>
-        <button className="postButton">send</button>
+        <button className="postButton" onClick={CreatePost}>
+          send
+        </button>
       </div>
     </div>
   );
