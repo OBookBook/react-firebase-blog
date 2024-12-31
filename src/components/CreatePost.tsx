13fa0d8f4b947752ab87801ee119ec256dee05ce
N.I <iworksWeb@gmail.com>
2024-12-31 10:38:31 +0900

    css CreatePostPage

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,7 +1,22 @@
-import React from "react";
+import "./CreatePost.css";
 
 const CreatePost = () => {
-  return <div>CreatePost</div>;
+  return (
+    <div className="createPostPage">
+      <div className="postContainer">
+        <h1>Post Page</h1>
+        <div className="inputPost">
+          <div>Title</div>
+          <input type="text" placeholder="add title" />
+        </div>
+        <div className="inputPost">
+          <div>Commet</div>
+          <textarea placeholder="add comment" />
+        </div>
+        <button className="postButton">send</button>
+      </div>
+    </div>
+  );
 };
 
 export default CreatePost;
