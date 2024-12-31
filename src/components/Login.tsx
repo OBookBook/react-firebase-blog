95847acc715bed055f6237a2f71912395d8d065f
N.I <iworksWeb@gmail.com>
2024-12-31 09:11:58 +0900

    add Authentication

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../firebase";
 
 const Login = () => {
-  return <div>Login</div>;
+  const loginInWithGoogle = () => {
+    signInWithPopup(auth, provider).then((result) => {
+      console.log(result);
+    });
+  };
+
+  return (
+    <div>
+      <p>Login</p>
+      <button onClick={loginInWithGoogle}>Googleにログイン</button>
+    </div>
+  );
 };
 
 export default Login;
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,11 @@
 /// <reference types="vite/client" />
 interface ImportMetaEnv {
-  readonly REACT_APP_FIREBASE_API_KEY: string;
-  readonly REACT_APP_FIREBASE_AUTH_DOMAIN: string;
-  readonly REACT_APP_FIREBASE_PROJECT_ID: string;
-  readonly REACT_APP_FIREBASE_STORAGE_BUCKET: string;
-  readonly REACT_APP_FIREBASE_MESSAGING_SENDER_ID: string;
-  readonly REACT_APP_FIREBASE_APP_ID: string;
+  readonly VITE_REACT_APP_FIREBASE_API_KEY: string;
+  readonly VITE_REACT_APP_FIREBASE_AUTH_DOMAIN: string;
+  readonly VITE_REACT_APP_FIREBASE_PROJECT_ID: string;
+  readonly VITE_REACT_APP_FIREBASE_STORAGE_BUCKET: string;
+  readonly VITE_REACT_APP_FIREBASE_MESSAGING_SENDER_ID: string;
+  readonly VITE_REACT_APP_FIREBASE_APP_ID: string;
 }
 
 interface ImportMeta {
