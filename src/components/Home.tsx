a18d7120fdefc3b75663a2cc40babb6323488f1d
N.I <iworksWeb@gmail.com>
2024-12-31 20:23:59 +0900

    add await getDocs

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,29 +1,48 @@
 import "./Home.css";
+import { useEffect, useState } from "react";
+import { db } from "../firebase";
+import { collection, getDocs } from "firebase/firestore";
+
+interface Post {
+  id: string;
+  title: string;
+  postsText: string;
+  author: {
+    username: string;
+  };
+}
 
 const Home = () => {
+  const [postList, setPostList] = useState<Post[]>([]);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      // https://firebase.google.com/docs/firestore/query-data/get-data?hl=ja&_gl=1*zpkj6l*_up*MQ..*_ga*NDg2MjQ1NTY4LjE3MzU2MTAzNzk.*_ga_CW55HF8NVT*MTczNTYxMDM3OS4xLjAuMTczNTYxMDM3OS4wLjAuMA..#get_a_document]
+      const data = await getDocs(collection(db, "posts"));
+      setPostList(
+        data.docs.map((doc) => ({
+          ...doc.data(),
+          id: doc.id,
+        })) as Post[]
+      );
+    };
+    getPosts();
+  }, []);
+
   return (
     <div className="homePage">
-      <div className="postContents">
-        <div className="postHeader">
-          <h1>Title</h1>
+      {postList.map((post) => (
+        <div key={post.id} className="postContents">
+          <div className="postHeader">
+            <h1>{post.title}</h1>
+          </div>
+          <div className="postTextContainer">{post.postsText}</div>
+          <div className="nameAndDeleteBtn">
+            <h3>@{post.author.username}</h3>
+            <button>delete</button>
+          </div>
         </div>
-      </div>
-      <div className="postTextContainer">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptatem
-        voluptas in neque consectetur, error aut nostrum modi tenetur iusto
-        pariatur iure, nihil labore dolores ipsa sequi illum! Corporis, placeat
-        consequatur! Minima natus eligendi, consequuntur eius alias ab, a enim
-        quidem, nisi quos facilis in pariatur dicta dolorum quod odit odio
-        consequatur perspiciatis! Praesentium assumenda dolor illo architecto ab
-        provident commodi. Est nesciunt, necessitatibus sunt sed eligendi soluta
-        incidunt, aliquam quaerat ad facilis explicabo hic adipisci at
-        temporibus, repellat ipsam quod possimus. Minima enim dolore odit a
-        consectetur eum nobis expedita!
-      </div>
-      <div className="nameAndDeleteBtn">
-        <h3>@Name</h3>
-        <button>delete</button>
-      </div>
+      ))}
     </div>
   );
 };
