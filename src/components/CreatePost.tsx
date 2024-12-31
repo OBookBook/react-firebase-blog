cd9ccd4018414b23c5ed5e5c4defd99a608642c3
N.I <iworksWeb@gmail.com>
2024-12-31 16:49:10 +0900

    add POST Cloud FireStore

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,13 +1,30 @@
-import { useState } from "react";
 import "./CreatePost.css";
+import { useState } from "react";
+import { db, auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
+import { collection, addDoc } from "firebase/firestore";
 
 const CreatePost = () => {
+  const navigate = useNavigate();
   const [title, setTitle] = useState<string>("");
   const [postText, setPostText] = useState<string>("");
 
-  const CreatePost = () => {
-    console.log(title);
-    console.log(postText);
+  const CreatePost = async () => {
+    if (!auth.currentUser) {
+      alert("You must be logged in to create a post");
+      return;
+    }
+
+    // @URL https://firebase.google.com/docs/firestore/manage-data/add-data?hl=ja&_gl=1*1b5wgtw*_up*MQ..*_ga*NDg2MjQ1NTY4LjE3MzU2MTAzNzk.*_ga_CW55HF8NVT*MTczNTYxMDM3OS4xLjAuMTczNTYxMDM3OS4wLjAuMA..#add_a_document
+    await addDoc(collection(db, "posts"), {
+      title: title,
+      postsText: postText,
+      author: {
+        username: auth.currentUser?.displayName,
+        id: auth.currentUser?.uid,
+      },
+    });
+    navigate("/");
   };
 
   return (
